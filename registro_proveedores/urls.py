2b456ad568d3e860b629e10f73065e1db85ba076
Felipe Garcia <juangarcia8254190235@gmail.com>
2025-10-27 09:50:28 -0500

    Redirección inicial hacia /cliente/nuevo/

--- registro_proveedores/urls.py
+++ registro_proveedores/urls.py
@@ -1,13 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include  # include para las rutas de apps
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('clientes.urls')),  # todas las rutas de la app clientes aquí
+    path('', RedirectView.as_view(url='/cliente/nuevo/', permanent=False)),
+    path('cliente/', include('clientes.urls')),
 ]
-
-# Para servir archivos media en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
